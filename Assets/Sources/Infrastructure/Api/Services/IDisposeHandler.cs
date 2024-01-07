76b032e41cc95453159fb76ba3060fa4e4495d0d
Klavikus <912654q@gmail.com>
2024-01-07 14:53:07 +0300

    Add dispose handling according to scene state

---
+++ Assets/Sources/Infrastructure/Api/Services/IDisposeHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sources.Infrastructure.Api.Services
+{
+    public interface IDisposeHandler
+    {
+        void Register(IDisposable disposable);
+        void Unregister(IDisposable disposable);
+        void DisposeAll();
+        void Clear();
+    }
+}
---
+++ Assets/Sources/Infrastructure/Core/Services/SceneDisposeHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Sources.Infrastructure.Api.Services;
+using UnityEngine;
+
+namespace Sources.Infrastructure.Core.Services
+{
+    public class SceneDisposeHandler : MonoBehaviour, IDisposeHandler
+    {
+        private readonly HashSet<IDisposable> _disposables = new();
+
+        public void Register(IDisposable disposable) =>
+            _disposables.Add(disposable);
+
+        public void Unregister(IDisposable disposable) =>
+            _disposables.Remove(disposable);
+
+        public void DisposeAll()
+        {
+            foreach (IDisposable disposable in _disposables)
+                disposable.Dispose();
+
+            Clear();
+        }
+
+        public void Clear() =>
+            _disposables.Clear();
+
+        private void OnDestroy() =>
+            DisposeAll();
+    }
+}
